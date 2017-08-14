2e091f2a606c701e8ec03f0854cbefa87d8ddd28
Tony <saric.tony@gmail.com>
2017-08-14 00:16:35 -0700

    updates to redux

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  root 'marknoce#homepage'
-
-  get '/homepage' => 'marknoce#homepage'
-  get '/about' => 'marknoce#about'
-  get '/books' => 'marknoce#books'
-  get '/blog' => 'marknoce#blog'
-  get '/appearances' => 'marknoce#appearances'
-  get '/contact' => 'marknoce#contact'
+  get 'hello_world', to: 'hello_world#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
